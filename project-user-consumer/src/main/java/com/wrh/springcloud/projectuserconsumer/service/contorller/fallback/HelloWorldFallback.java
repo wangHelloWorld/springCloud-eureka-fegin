b16114a2d1cdbc1a82c7072d5611561f9e4ad2c6
wangHelloWorld <104488954@qq.com>
2019-09-01 01:47:45 +0800

    add hystrix and fallback class

---
+++ project-user-consumer/src/main/java/com/wrh/springcloud/projectuserconsumer/service/contorller/fallback/HelloWorldFallback.java
@@ -0,0 +1,23 @@
+package com.wrh.springcloud.projectuserconsumer.service.contorller.fallback;
+
+import org.springframework.stereotype.Component;
+
+import com.wrh.springcloud.projectcommon.common.User;
+import com.wrh.springcloud.projectuserconsumer.service.interface1.HelloWorldIn;
+
+@Component
+public class HelloWorldFallback implements HelloWorldIn{
+
+	@Override
+	public String login() {
+		// TODO Auto-generated method stub
+		return "login exception";
+	}
+
+	@Override
+	public String loginUser(User user) {
+		// TODO Auto-generated method stub
+		return "loginUser exception";
+	}
+
+}
