7f8734e5b68d523ed2f62084ef2d3c7d6edfab0d
wangHelloWorld <104488954@qq.com>
2019-08-31 18:48:49 +0800

    add @ReqeustBody in consumer contorller

--- project-user-consumer/src/main/java/com/wrh/springcloud/projectuserconsumer/service/contorller/HelloWorldController.java
+++ project-user-consumer/src/main/java/com/wrh/springcloud/projectuserconsumer/service/contorller/HelloWorldController.java
@@ -8,23 +8,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloWorldController {
 
-    @Autowired
-    private HelloWorldIn helloWorld;
+	@Autowired
+	private HelloWorldIn helloWorld;
 
-    @RequestMapping(value = "/loginC",method = RequestMethod.GET)
-    public String loginC(){
+	@RequestMapping(value = "/loginC", method = RequestMethod.GET)
+	public String loginC() {
 
-            return helloWorld.login();
+		return helloWorld.login();
 
-    }
-    @RequestMapping(value = "/loginUser",method = RequestMethod.POST)
-    public String loginCUser(@RequestParam("name") String name, @RequestParam("password") String password){
-        System.out.println("name " + name + " password "+ password);
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        return helloWorld.loginUser(user);
+	}
 
-    }
+	@RequestMapping(value = "/loginUser", method = RequestMethod.POST)
+	public String loginCUser(@RequestParam("name") String name, @RequestParam("password") String password) {
+		System.out.println("name " + name + " password " + password);
+		User user = new User();
+		user.setName(name);
+		user.setPassword(password);
+		return helloWorld.loginUser(user);
+
+	}
+
+	@RequestMapping(value = "/loginUserBody", method = RequestMethod.POST)
+	public String loginCUserBody(@RequestBody User user) {
+		return helloWorld.loginUser(user);
+
+	}
 
 }
